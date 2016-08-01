427261fbcc58e1566d144584dc57948cdf491637
Sang Dang <zkidkid@gmail.com>
2016-08-01 15:54:36 +0700

    implement ssdb

--- storage/src/main/java/id/scommon/storage/impl/SSDB.java
+++ storage/src/main/java/id/scommon/storage/impl/SSDB.java
@@ -1,5 +1,11 @@
 package id.scommon.storage.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nutz.ssdb4j.SSDBs;
+import org.nutz.ssdb4j.spi.Response;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import id.scommon.storage.INoSQLDB;
@@ -7,33 +13,63 @@ import id.scommon.storage.INoSQLDB;
 /**
  * Created by SangDang on 8/1/16.
  */
-public class SSDB implements INoSQLDB{
+public class SSDB implements INoSQLDB {
+  protected String host;
+  protected int port;
+  protected int timeoutInMs;
+  protected org.nutz.ssdb4j.spi.SSDB ssdbClient;
+
+  public SSDB(String host, int port, int timeoutInMs) {
+    this.host = host;
+    this.port = port;
+    this.timeoutInMs = timeoutInMs;
+    initSSDBClient();
+  }
+
+  protected synchronized void initSSDBClient() {
+    try {
+      if (ssdbClient != null) {
+        ssdbClient.close();
+      }
+      ssdbClient = SSDBs.pool(host, port, timeoutInMs, null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
   public boolean put(byte[] key, byte[] value) {
-    return false;
+    return ssdbClient.set(key, value).ok();
   }
 
   public int mput(List<byte[]> key, List<byte[]> value) {
-    return 0;
+    if (key == null || value == null || key.size() != value.size()) return -2;
+    List<byte[]> pairs = new ArrayList<byte[]>();
+    for (int i = 0; i < key.size(); ++i) {
+      pairs.add(key.get(i));
+      pairs.add(value.get(i));
+    }
+    Response response = ssdbClient.multi_set(pairs);
+    return response.ok() == true ? 1 : -1;
   }
 
   public byte[] get(byte[] key) {
-    return new byte[0];
+    return ssdbClient.get(key).datas.get(0);
   }
 
   public List<byte[]> mget(List<byte[]> key) {
-    return null;
+    return ssdbClient.multi_get(key).datas;
   }
 
   public boolean isExist(byte[] key) {
-    return false;
+    return ssdbClient.exists(key).asInt() == 1;
   }
 
   public boolean delete(byte[] key) {
-    return false;
+    return ssdbClient.del(key).ok();
   }
 
   public int mdelete(List<byte[]> key) {
-    return 0;
+    return ssdbClient.multi_del(key).asInt();
   }
 
   public boolean removeAll() {
